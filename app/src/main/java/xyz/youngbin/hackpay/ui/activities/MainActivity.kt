37c6c14f7a7df7c965033b9eb0d4c5989ff13942
sukso96100 <sukso96100@gmail.com>
2017-11-23 21:59:19 +0900

    거래 내역 화면

--- app/src/main/java/xyz/youngbin/hackpay/ui/activities/MainActivity.kt
+++ app/src/main/java/xyz/youngbin/hackpay/ui/activities/MainActivity.kt
@@ -8,6 +8,7 @@ import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 import xyz.youngbin.hackpay.R
 
 class MainActivity : AppCompatActivity() {
@@ -16,13 +17,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        payment.setOnClickListener{
             startActivity(Intent(this, ScannerActivity::class.java))
-
         }
+        credits.setOnClickListener {
+            startActivity(Intent(this, CreditsAndHistoryActivity::class.java))
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
